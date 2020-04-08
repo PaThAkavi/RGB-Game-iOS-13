c74c1f68395f40eb7bc4cc7bfe551098d1e000ef
Avaneesh <pathakavaneesh@gmail.com>
2020-04-09 00:12:39 +0530

    Add reload button functionality

--- RGB Game/GameViewController.swift
+++ RGB Game/GameViewController.swift
@@ -10,20 +10,63 @@ import UIKit
 
 class GameViewController: UIViewController {
 
+    @IBOutlet weak var btn1: UIButton!
+    @IBOutlet weak var btn2: UIButton!
+    @IBOutlet weak var btn3: UIButton!
+    @IBOutlet weak var btn4: UIButton!
+    @IBOutlet weak var btn5: UIButton!
+    @IBOutlet weak var btn6: UIButton!
+    @IBOutlet weak var requiredColor: UILabel!
+    
+    var buttons = [UIButton]()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        buttons = [btn1, btn2, btn3, btn4, btn5, btn6]
+        generateNewColors()
+        reloadBoxColour()
+        
     }
-    
     
     @IBAction func colorBlockPressed(_ sender: UIButton) {
         
+        let rgbArr: [Int] = randomRGBGenerator()
+        sender.backgroundColor = UIColor(red: CGFloat(rgbArr[0])/255.0, green: CGFloat(rgbArr[1])/255.0, blue: CGFloat(rgbArr[2])/255.0, alpha: 1.0)
+        
+    }
+    
+    @IBAction func reloadButtonPressed(_ sender: UIButton) {
+        generateNewColors()
+        reloadBoxColour()
+    }
+    
+    //r, g, b value generator
+    func randomRGBGenerator() -> [Int] {
         let r = arc4random_uniform(256)
         let g = arc4random_uniform(256)
         let b = arc4random_uniform(256)
-        sender.backgroundColor = UIColor(red: CGFloat(r)/255.0, green: CGFloat(g)/255.0, blue: CGFloat(b)/255.0, alpha: 1.0)
+        var rgb = [Int]()
+        rgb.append(Int(r))
+        rgb.append(Int(g))
+        rgb.append(Int(b))
+        return rgb
+    }
+    
+    //Generates random r,g,b values
+    func generateNewColors() {
         
+        let rgbArr: [Int] = randomRGBGenerator()
+        requiredColor.text = "RGB(\(rgbArr[0]), \(rgbArr[1]), \(rgbArr[2]))"
+        
+    }
+    
+    //Changes the box colors
+    func reloadBoxColour() {
+        for i in 0...5 {
+            let rgbArr: [Int] = randomRGBGenerator()
+            buttons[i].backgroundColor = UIColor(red: CGFloat(rgbArr[0])/255.0, green: CGFloat(rgbArr[1])/255.0, blue: CGFloat(rgbArr[2])/255.0, alpha: 1.0)
+        }
     }
     
 }
